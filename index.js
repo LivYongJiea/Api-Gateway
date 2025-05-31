2724365367b3481eefa4f2e56c68139b7c429d56
LivYongJiea <jieaisking44@gmail.com>
2025-05-31 19:11:18 +0700

    Update

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer({});
 //redirect to the student micro-service
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req,res, {target:'http://localhost:5002'});
+    proxy.web(req,res, {target:'http://184.73.78.40:5002'});
 })
 
 //redirect to the teacher micro-service
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req,res, {target:'http://localhost:5003'});
+    proxy.web(req,res, {target:'http://3.83.177.15:5003'});
 })
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(4007, () => {
